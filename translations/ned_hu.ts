ef4b27181dafae8095992343228bde64b003dbfc
hhahhahh1 <hhahhahh@protonmail.com>
2024-07-17 11:07:59 +0300

    Update footer links

---
+++ translations/ned_hu.ts
@@ -0,0 +1,434 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hu_HU">
+<context>
+    <name>FindDialog</name>
+    <message>
+        <location filename="../src/FindDialog.cpp" line="56"/>
+        <source>Find</source>
+        <translation>Keresés</translation>
+    </message>
+    <message>
+        <source>&amp;Find</source>
+        <translation type="obsolete">&amp;Keres</translation>
+    </message>
+    <message>
+        <location filename="../src/FindDialog.cpp" line="62"/>
+        <source>Find &amp;next &gt;&gt;</source>
+        <translation>Előre &amp;keres &gt;&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/FindDialog.cpp" line="63"/>
+        <source>Find &amp;previous &lt;&lt;</source>
+        <translation>&amp;Hátrafelé keres &lt;&lt;</translation>
+    </message>
+    <message>
+        <location filename="../src/FindDialog.cpp" line="64"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Bezár</translation>
+    </message>
+    <message>
+        <location filename="../src/FindDialog.cpp" line="66"/>
+        <source>Find &amp;what</source>
+        <translation>Keresd &amp;ezt:</translation>
+    </message>
+    <message>
+        <location filename="../src/FindDialog.cpp" line="67"/>
+        <source>Case &amp;sesitive</source>
+        <translation>&amp;Nagybetű érzékeny</translation>
+    </message>
+    <message>
+        <source>Search &amp;Backward</source>
+        <translation type="obsolete">&amp;Visszafelé keres</translation>
+    </message>
+    <message>
+        <location filename="../src/FindDialog.cpp" line="68"/>
+        <source>Find w&amp;hole words</source>
+        <translation>&amp;Teljes szavakat keres</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>%1 - Ned %2.%3</source>
+        <translation type="obsolete">%1 - Ned %2.%3</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="44"/>
+        <location filename="../src/MainWindow.cpp" line="385"/>
+        <source>%1[*] - Ned</source>
+        <translation>%1[*] - Ned</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="44"/>
+        <location filename="../src/MainWindow.cpp" line="379"/>
+        <source>Untitled</source>
+        <translation>Névtelen</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="61"/>
+        <source>&amp;New</source>
+        <translation>&amp;Új</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="65"/>
+        <source>Create new document</source>
+        <translation>Új doukumentum létrehozása (új ablakban)</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="68"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Megnyitás</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="72"/>
+        <source>Open an exsisiting document</source>
+        <translation>Egy létező dokumentum megnyitása</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="75"/>
+        <source>&amp;Save</source>
+        <translation>Menté&amp;s</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="79"/>
+        <source>Save the edited document</source>
+        <translation>Dokumentum elmentése</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="82"/>
+        <source>&amp;Save As</source>
+        <translation>Me&amp;ntés másként</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="86"/>
+        <source>Save as a document</source>
+        <translation>A dokumentum mentése más néven</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="89"/>
+        <source>&amp;Undo</source>
+        <translation>&amp;Visszavonás</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="94"/>
+        <source>Undo a change in the editor area</source>
+        <translation>Egy változás visszavonása</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="98"/>
+        <source>&amp;Redo</source>
+        <translation>&amp;Ismét</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="103"/>
+        <source>Redo a change in the editor area</source>
+        <translation>Visszavonás visszavonása</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="107"/>
+        <source>&amp;Find...</source>
+        <translation>&amp;Keresés...</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="110"/>
+        <source>Find a text</source>
+        <translation>Szöveg keresése</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="113"/>
+        <source>Cl&amp;ear All</source>
+        <translation>Összes &amp;törlése</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="120"/>
+        <source>&amp;Copy</source>
+        <translation>&amp;Másolás</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="129"/>
+        <source>C&amp;ut</source>
+        <translation>&amp;Kivágás</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="171"/>
+        <source>Select font ...</source>
+        <translation>&amp;Betűtípus kiválasztása...</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="172"/>
+        <source>Select the font for the text area</source>
+        <translation>A betűtípus kiválasztása a szöveghez</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="175"/>
+        <source>Set to defaults</source>
+        <translation>Alapértelmetett értékek visszaállítása</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="176"/>
+        <source>Set everything to it&apos;s default setting, like font</source>
+        <translation>Minden beállítást visszaállít az alapértelmezett értékeire, például betűtípus</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="243"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Beállítások</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="496"/>
+        <source>&lt;b&gt;&lt;h3&gt;Ned, version %1&lt;/h3&gt;&lt;/b&gt;Ned is a simple, cross-platform text editor written in C++, using the Qt4 graphical framework.&lt;br&gt;&lt;br&gt;&lt;i&gt;Copyright (C) Akos Kovacs - 2010&lt;/i&gt;</source>
+        <translation>&lt;b&gt;&lt;h3&gt;Ned, verzió %1&lt;/h3&gt;&lt;/b&gt;A Ned egy egyszerű, multiplatform, grafikus szövegszerkesztő. C++-ban íródott, a Qt grafikus toolkit használatával&lt;br&gt;&lt;i&gt;Copyright (C) Akos Kovacs - 2010&lt;/i&gt;
+ {3&gt;?} {1&lt;?} {3&gt;?} {4 ?} {2010&lt;?}</translation>
+    </message>
+    <message>
+        <source>Ready....</source>
+        <translation type="obsolete">Kész...</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="415"/>
+        <location filename="../src/MainWindow.cpp" line="443"/>
+        <source>Every file (*)
+Text files (*.txt)</source>
+        <translation>Minden fájl (*)
+Szöveg fájlok (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="465"/>
+        <source>Every file (*.*)
+Text files (*.txt)</source>
+        <translation>Minden fájl (*.*)
+Szöveg fájlok (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="427"/>
+        <source>File loading cancelled</source>
+        <translation>Fájlbetöltés megszakítva</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="283"/>
+        <source>Total characters: %1</source>
+        <translation>Összes karakter: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="285"/>
+        <source>Total lines: %1</source>
+        <translation>Összes sor: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="290"/>
+        <source>Access: Read only</source>
+        <translation>Hozzáférés: csak olvasható</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="292"/>
+        <source>Access: RW</source>
+        <translation>Hozzáférés: írható-olvasható</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="431"/>
+        <source>File loaded</source>
+        <translation>Fájl betöltve</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;&lt;h3&gt;Ned, version %1.%2&lt;/h3&gt;&lt;/b&gt;Ned is a simple, cross-platform text editor written in C++, using the Qt4 graphical framework.&lt;br&gt;&lt;br&gt;&lt;i&gt;Copyright (C) Akos Kovacs - 2010&lt;/i&gt;</source>
+        <translation type="obsolete">&lt;b&gt;&lt;h3&gt;Ned, verzió %1.%2&lt;/h3&gt;&lt;/b&gt;A Ned egy egyszerű, multiplatform, grafikus szövegszerkesztő. C++-ban íródott, a Qt grafikus toolkit használatával&lt;br&gt;&lt;i&gt;Copyright (C) Akos Kovacs - 2010&lt;/i&gt;
+</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="510"/>
+        <source>An error occured when trying to read %1 file from the disk</source>
+        <translation>Hiba történt a %1 fájl olvasása közben</translation>
+    </message>
+    <message>
+        <source>&amp;Clear</source>
+        <translation type="obsolete">&amp;Törlés</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="117"/>
+        <source>Delete a part of a text</source>
+        <translation>Egy kijelölt részlet törlése a szövegből</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation type="obsolete">Másolás</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="125"/>
+        <source>Copy a text part</source>
+        <translation>Kijelölt szövegrész másolása</translation>
+    </message>
+    <message>
+        <source>&amp;Cut</source>
+        <translation type="obsolete">&amp;Kivágás</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="134"/>
+        <source>Cut a text part</source>
+        <translation>Egy kijelölt szövegrész kivágása</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="138"/>
+        <location filename="../src/MainWindow.cpp" line="240"/>
+        <source>&amp;Paste</source>
+        <translation>&amp;Beillesztés</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="142"/>
+        <source>Paste the cutted text</source>
+        <translation>Szöveg beillesztése az aktuális kurzor-pozíció után</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="145"/>
+        <source>&amp;Select All</source>
+        <translation>&amp;Minden kijelölése</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="149"/>
+        <source>Select all text in the text editor</source>
+        <translation>Az összes szöveg kijelölése</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="152"/>
+        <source>About this</source>
+        <translation>Az alkalmazásról</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="154"/>
+        <source>About this application</source>
+        <translation>Az alkalmazás névjegyének-ablakának megjelenítése</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="157"/>
+        <source>Exit</source>
+        <translation>&amp;Kilépés</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="160"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="161"/>
+        <source>Exit from the application</source>
+        <translation>Kilépés az alkalmazásból</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="164"/>
+        <source>Pate Date and Time</source>
+        <translation>Dátum és idő beillesztése</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="167"/>
+        <source>Ctrl+D</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="168"/>
+        <source>Paste the current date and time</source>
+        <translation>Beilleszti a pontos időt a kurzor pozíciója után</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="199"/>
+        <source>&amp;Base</source>
+        <translation>&amp;Alap</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="204"/>
+        <location filename="../src/MainWindow.cpp" line="227"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Szerkesztés</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="219"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fájl</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="247"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Segítség</translation>
+    </message>
+    <message>
+        <source>Every file (*.*)</source>
+        <translation type="obsolete">Minden fájl (*.*)</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="495"/>
+        <source>About Ned</source>
+        <translation>A Nedről</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;&lt;h3&gt;Ned, version: %1.%2&lt;/h3&gt;&lt;/b&gt;Ned is a simple, cross-platform text editor written in C++, using the Qt4 graphical framework.&lt;br&gt;&lt;br&gt;&lt;i&gt;Copyright (C) Akos Kovacs - 2010&lt;/i&gt;</source>
+        <translation type="obsolete">&lt;b&gt;&lt;h3&gt;Ned, verzió: %1.%2&lt;/h3&gt;&lt;/b&gt;A Ned egy egyszerű c++-ban írt szövegszerkesztő, ami a Qt4-es grafikus keretrendszert használja.&lt;br&gt;&lt;br&gt;&lt;i&gt;Copyright (C) Kovács Ákos - 2010&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>%1[*] - Ned %2.%3</source>
+        <translation type="obsolete">%1[*] - Ned %2.%3</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="396"/>
+        <source>Ned</source>
+        <translation>Ned</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="397"/>
+        <source>The document has been modified.
+Do you want to save the changes?</source>
+        <translation>A dokumentum módosult.
+Szeretné menteni a változásokat?</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="414"/>
+        <source>Open text files</source>
+        <translation>Egyszerű szöveg fájlok</translation>
+    </message>
+    <message>
+        <source>Text files (*.txt)</source>
+        <translation type="obsolete">Szövegfájlok (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="442"/>
+        <source>Save text files</source>
+        <translation>Szövegfájl mentése</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="449"/>
+        <source>Ned - Error</source>
+        <translation>Ned - Hiba</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="449"/>
+        <source>The file is not writable</source>
+        <translation>A fálj nem írható</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="464"/>
+        <source>Save as text file</source>
+        <translation>Szövegfájl mentése más néven</translation>
+    </message>
+    <message>
+        <source>About - Ned</source>
+        <translation type="obsolete">Névjegy - Ned</translation>
+    </message>
+    <message>
+        <source>Ned is a simple text editor written in C++, using the Qt4 graphical framework Copyright (C) Akos Kovacs - 2010</source>
+        <translation type="obsolete">Ned egy egyszerű szöveg-szerkesztő (editor) C++-ban írva. A Qt4-es grafikus keretrendszert használja. Copyright (C) Ákos Kovács - 2010</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="509"/>
+        <location filename="../src/MainWindow.cpp" line="524"/>
+        <source>Critical Error - Ned</source>
+        <translation>Kritikus hiba - Ned</translation>
+    </message>
+    <message>
+        <source>An error occured when trying to read%1 file from the disk</source>
+        <translation type="obsolete">Hiba történt a %1 fájl megnyitása közben</translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="525"/>
+        <source>An error occured when trying to write%1 file to the disk</source>
+        <translation>Hiba történt a %1 fájl írása közben</translation>
+    </message>
+</context>
+</TS>
